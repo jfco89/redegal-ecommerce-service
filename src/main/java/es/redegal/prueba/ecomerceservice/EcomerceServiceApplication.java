607e1670ab0a1f80151a0a8db5d4d92db01a3162
JuanFco <juanfcovalj@gmail.com>
2022-11-21 16:21:43 +0100

    Inicializacion proyecto ecommerce-service

---
+++ src/main/java/es/redegal/prueba/ecomerceservice/EcomerceServiceApplication.java
@@ -0,0 +1,13 @@
+package es.redegal.prueba.ecomerceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/redegal/prueba/ecomerceservice/EcomerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.redegal.prueba.ecomerceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
